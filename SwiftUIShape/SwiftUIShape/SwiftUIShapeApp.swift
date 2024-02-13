94aad303331895eac42c54e7f2fc5919b39a772a
elliotmyers <elliotmyers47@gmail.com>
2024-02-13 14:23:12 +0300

    SwiftUIShape Created

---
+++ SwiftUIShape/SwiftUIShape/SwiftUIShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIShapeApp.swift
+//  SwiftUIShape
+//
+//  Created by Raiden Yamato on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
