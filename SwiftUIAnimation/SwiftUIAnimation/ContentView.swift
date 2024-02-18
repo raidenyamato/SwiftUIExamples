0aa57e6c0dfe398bf46933743bdec0fc91ad3e25
elliotmyers <elliotmyers47@gmail.com>
2024-02-18 13:56:04 +0300

    Animations finished

--- SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
+++ SwiftUIAnimation/SwiftUIAnimation/ContentView.swift
@@ -14,6 +14,11 @@ struct ContentView: View {
     
     @State private var isLoading = false
     
+    @State private var progress: CGFloat = 0.0
+    
+    @State private var recordBegin = false
+    @State private var recording = false
+    
     var body: some View {
         
         ScrollView {
@@ -32,10 +37,10 @@ struct ContentView: View {
                 // implicit animation
                 .animation(.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3), value: circleColorChanged)
                 .onTapGesture {
-                   
-                        circleColorChanged.toggle()
-                        heartColorChanged.toggle()
-                        heartSizeChanged.toggle()
+                    
+                    circleColorChanged.toggle()
+                    heartColorChanged.toggle()
+                    heartSizeChanged.toggle()
                     
                 }
                 //// 2
@@ -64,34 +69,36 @@ struct ContentView: View {
                     Circle()
                         .stroke(Color(.systemGray5), lineWidth: 14)
                         .frame(width: 100, height: 100)
-                        
+                    
                     
                     Circle()
                         .trim(from: 0, to: 0.2)
                         .stroke(Color.green, lineWidth: 5)
                         .frame(width: 100, height: 100)
                         .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
-                       
-    //                    .animation(.default.repeatForever(autoreverses: false), value: isLoading)
-                        .animation(.linear(duration: 4).repeatForever(autoreverses: false), value: isLoading)
+                    
+                    //  .animation(.default.repeatForever(autoreverses: false), value: isLoading)
+                        .animation(
+                            .linear(duration: 4)
+                            .repeatForever(autoreverses: false), value: isLoading)
                         .onAppear() {
                             // change to true for animating
-
+                            
                         }
-                        
+                    
                 }
-            
+                
                 
                 
                 
                 ZStack {
-                        
+                    
                     Text("Loading")
                         .font(.system(.body, design: .rounded))
                         .bold()
                         .offset(x: 0, y: -25)
-
-
+                    
+                    
                     RoundedRectangle(cornerRadius: 3)
                         .fill(Color(.systemGray5))
                         .frame(width: 250, height: 3)
@@ -107,10 +114,84 @@ struct ContentView: View {
                 .padding(.top, 40)
                 
                 
+                
+                ZStack {
+                    Text("\(Int(progress * 100))%")
+                        .font(.system(.title, design: .rounded))
+                        .bold()
+                    
+                    Circle()
+                        .stroke(Color(.systemGray5), lineWidth: 10)
+                        .frame(width: 150, height: 150)
+                    
+                    Circle()
+                        .trim(from: 0, to: progress)
+                        .stroke(Color.green, lineWidth: 10)
+                        .frame(width: 150, height: 150)
+                        .rotationEffect(Angle(degrees: -90))
+                }
+                .onAppear {
+                    Timer.scheduledTimer(withTimeInterval: 1.5, repeats: true) { timer in
+                        self.progress += 0.05
+                        print(self.progress)
+                        if self.progress >= 1.0 {
+                            timer.invalidate()
+                        }
+                    }
+                }
+                
+                
+                HStack {
+                    ForEach(0...4, id: \.self) { index in
+                        Circle()
+                            .frame(width: 10, height: 10)
+                            .foregroundColor(.green)
+                            .scaleEffect(self.isLoading ? 0 : 1)
+                            .animation(.linear(duration: 0.6).repeatForever().delay(0.2 * Double(index)), value: isLoading)
+                    }
+                    
+                    
+                }
+                
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: recordBegin ? 30 : 5)
+                        .frame(width: recordBegin ? 60 : 230, height: 60)
+                        .foregroundColor(recordBegin ? .red : .green)
+                        .overlay {
+                            Image(systemName: "mic.fill")
+                                .font(.system(.title))
+                                .foregroundColor(.white)
+                                .scaleEffect(recording ? 0.7 : 1)
+                                .animation(Animation.spring().repeatForever().delay(0.5), value: recording)
+                            
+                        }
+                    
+                    RoundedRectangle(cornerRadius: recordBegin ? 35 : 10)
+                        .trim(from: 0, to: recordBegin ? 0.0001 : 1)
+                        .stroke(lineWidth: 5)
+                        .frame(width: recording ? 70 : 240, height: 70)
+                        .foregroundColor(.green)
+                }
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        self.recordBegin.toggle()
+                    }
+                    
+                   
+                        self.recording.toggle()
+                   
+                    
+                }
+                
+                
             }
             .onAppear {
                 isLoading = true
             }
+            
+            
+            
             
         }
     }
