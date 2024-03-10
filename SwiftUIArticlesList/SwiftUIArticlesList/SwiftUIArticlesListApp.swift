1feeca57b344d555ad6d92fd7599e055729cf26e
elliotmyers <elliotmyers47@gmail.com>
2024-03-10 20:37:32 +0300

    first  commit

---
+++ SwiftUIArticlesList/SwiftUIArticlesList/SwiftUIArticlesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIArticlesListApp.swift
+//  SwiftUIArticlesList
+//
+//  Created by Raiden Yamato on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIArticlesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
